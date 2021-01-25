26058f988b6d68df30e70a32fd11ed0885b7b9c7
ahmad raihan <gedehuap02@gmail.com>
2021-01-25 23:30:31 +0700

    Initial commit

---
+++ app/src/main/java/com/ahmadraihan/loginsqlite/DataModelUser.kt
@@ -0,0 +1,4 @@
+package com.ahmadraihan.loginsqlite
+
+data class DataModelUser (var email: String ="", var pass: String ="", var
+fullname: String ="", var alamat: String= "", var jeniskelamin: String = "")
